896c6c3b9dfde40825c8c6b7a033ec5f86ab5403
Emmanuel Casas <emmanuelcasas@MacBook-Pro-de-Emmanuel.local>
2018-08-12 18:44:48 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (textToScream) => {
+    return textToScream.toUpperCase();
+})
+
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append server.log.');
+        }
+    });
+    console.log(log);
+    next();
+});
+
+//app.use((req, res, next) =>{
+//   res.render('maintenance.hbs'); 
+//});
+
+app.get('/', (req, res) => {
+    //res.send('Hello Express!');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Emmanuel hey'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to fulfill this request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is ready in port 3000');
+});
